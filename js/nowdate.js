af97ff96c5ef5ffc7fb43fe617e2817f6fe76616
이름 <>
2024-07-18 23:19:27 +0900

    날짜에 요일추가

--- js/nowdate.js
+++ js/nowdate.js
@@ -1,8 +1,19 @@
-const h4now = document.querySelector(".now_wrap h4")
+const h4now = document.querySelector(".now_wrap h4");
 
-const now = new Date()
-const nowYear = now.getFullYear()
-const nowMonth = String(now.getMonth()+1).padStart(2, "0");
-const nowDate = String(now.getDate()).padStart(2, "0")
+const now = new Date();
+const nowYear = now.getFullYear();
+const nowMonth = String(now.getMonth() + 1).padStart(2, "0");
+const nowDate = String(now.getDate()).padStart(2, "0");
 
-h4now.innerText = `${nowYear}년 ${nowMonth}년 ${nowDate}일`
+const days = `${nowYear}-${nowMonth}-${nowDate}`;
+
+const dow = getDayOfWeek(days);
+
+function getDayOfWeek(days) {
+  const week = ["일", "월", "화", "수", "목", "금", "토"];
+  const dayOfWeek = week[new Date(days).getDay()];
+
+  return `[${dayOfWeek}]`;
+}
+
+h4now.innerText = `${nowYear}년 ${nowMonth}년 ${nowDate}일 ${dow}`;
